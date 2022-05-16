dbd610fc0e285e549fe49ec41c863f6fba417888
Horvath9 <pedro.montemurrog1@gmail.com>
2022-05-16 15:09:00 -0300

    bug fix

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <br></br>
+        ai minha vuaidaaaaaa
+        <img ></img>
         <a
           className="App-link"
           href="https://reactjs.org"
